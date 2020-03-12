8caba62f38f2fb28a63d6881e8fcc69b2cc99de8
andech <7578191@gmail.com>
2020-03-12 22:30:07 +0300

    Updates

---
+++ precache-manifest.c0292398489ec1073bb402c9bc58843a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f01b04535c80e24ffd6cf334324c1ae",
+    "url": "/static1/index.html"
+  },
+  {
+    "revision": "1bf5d0edb06f623f3758",
+    "url": "/static1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d30c3bf90c5eb18078a0",
+    "url": "/static1/static/js/2.d137b91b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static1/static/js/2.d137b91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf5d0edb06f623f3758",
+    "url": "/static1/static/js/main.bb368650.chunk.js"
+  },
+  {
+    "revision": "3d3200f76c0908627f8c",
+    "url": "/static1/static/js/runtime-main.69a673ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static1/static/media/logo.5d5d9eef.svg"
+  }
+]);
